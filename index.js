0142d8f6aeeda3c3b4dfe83bad40c409f0bc56b1
Cory <corystephens012@gmail.com>
2021-06-04 14:32:54 -0400

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-// Write your code here!
+const main = document.getElementById("main")
+
+main.remove();
+
+const newHeader = document.createElement("h1")
+
+newHeader.innerHTML = "Cory is the champion";
+newHeader.id = "victory"
+
+document.body.appendChild(newHeader)
